1667c2b5dee3aa76a00363a11fe841e85be34256
Huseyin Can İlhan <canilhn1@gmail.com>
2022-02-18 12:44:27 +0300

    1.version

--- src/App.js
+++ src/App.js
@@ -1,27 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
-import User from './components/User'
+
 import Navbar from './components/navbar';
 function App() {
   const cont=true;
   return (
     <div className="App">
       <Navbar />
-      <hr/>
-      <User 
-      name=" can"
-      bölüm=" tamirci"
-      calistigiyer=" tamirci yanı "
-      />
-      <hr/>
-      <Navbar  a=" bu da iki"/>
-
-      <User 
-      name=" can"
-      bölüm=" tamirci"
-      calistigiyer=" tamirci yanı "
-      fazla=" bu ikinci de görüncek"
-      />
+     
+      
+      
+      
       
       
     </div>
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,21 +1,227 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-
+import {FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { FaBeer } from "@react-icons/all-files";
+import { } from 'react-icons/';
 export default function navbar(props) {
   return (
     <div>
-            <h1> {props.title}</h1>
-            <h1> {props.a}</h1>
+    <nav class="navbar fixed-top navbar-expand-lg  navbar-light bg-light">
+    <a class="navbar-text " href="#"></a>
+
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+ 
+  <div class="collapse navbar-collapse " id="navbarNavAltMarkup">
+    <div class="navbar-nav col-12 dropdown ">
+      <a class="nav-item nav-link" href="#">Yazılarım <span class="sr-only">(current)</span></a>
+      <a class="nav-item nav-link" href="#">burda foto olcak <span class="sr-only">(current)</span></a>
+      <a class="nav-item nav-link" href="#">Ben Kimim  </a>
+      
+     
+    </div>
+    
+  </div>
+
+</nav>
+            <p>
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+              ldfgkidşlgkdsflşigkdsşgklisdşgklşisdfigkdşigldskgşidsgdlfigdf
+            </p>
     </div>
   )
-}
-navbar.propTypes = {
-    title:PropTypes.string.isRequired,
-    
-
-}
-navbar.defaultProps = 
-{
-    title:" default",
 }
